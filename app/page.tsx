81e2323768ee0152a21af4a3a2b7b2f0775f76f0
sarang <sarang.office01@gmail.com>
2025-10-28 10:12:30 +0530

    add guest component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
-export default function page() {
+import Guest from "@/components/Guest";
+import { currentUser } from "@clerk/nextjs/server";
+
+export default async function page() {
+  const user = await currentUser();
+
+  if (!user) {
+    return <Guest />;
+  }
+
   return <div className="text-red-500">Home Page</div>;
 }
